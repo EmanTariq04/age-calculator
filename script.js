b957562c0e0f1ee7f48a5271f046258dd32c97af
Eman Tariq <emantariq.1004@gmail.com>
2025-07-02 20:11:36 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+
+function calculateAge() {
+    console.log("Button clicked");
+}
+
+btnEl.addEventListener("click", calculateAge);
